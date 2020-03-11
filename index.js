a72254704fe6a0739e6c3d69f08e2ab1a6c64f1c
ryanadhi <ryanadhi.p@gmail.com>
2020-03-11 08:47:04 +0700

    modal + landing page

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('#avatar').hide();
+    $('#page-register').hide();
+    $('#page-login').hide();
+    $('#page-main').hide();
+    $('#btn-create').hide();
+    $('#btn-logout').hide();
+})
