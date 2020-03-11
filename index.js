b4c38e0b29d38bdb7fede8c8223f464482885acb
ryanadhi <ryanadhi.p@gmail.com>
2020-03-11 13:26:39 +0700

    trial fetch tasks

--- index.js
+++ index.js
@@ -1,9 +1,63 @@
-// $(document).ready(function () {
-//     $('#avatar').hide();
-//     $('#page-register').hide();
-//     $('#page-login').hide();
-//     $('#page-main').hide();
-//     $('#page-landing').show();
-//     $('#btn-create').hide();
-//     $('#btn-logout').hide();
-// })
+const app = new Vue ({
+    el : '#app',
+    data : {
+        isLogin : false,
+        currentPage : 'landingPage',
+        userData : {
+            name : '',
+            email : '',
+            password : '',
+        },
+        tasks : [
+            {
+                id : 1, title : 'This is new card (backlog)', category : 'backlog'
+            },
+            {
+                id : 2, title : 'This is new card (to do)', category : 'todo'
+            },
+            {
+                id : 3, title : 'This is new card (done)', category : 'done'
+            },
+            {
+                id : 4, title : 'This is new card (completed)', category : 'completed'
+            },
+            {
+                id : 5, title : 'This is new card', category : 'backlog'
+            },
+            {
+                id : 6, title : 'This is new card', category : 'backlog'
+            },
+            {
+                id : 7, title : 'This is new card', category : 'backlog'
+            },
+            {
+                id : 8, title : 'This is new card', category : 'backlog'
+            },
+            {
+                id : 9, title : 'This is new card', category : 'done'
+            },
+            {
+                id : 10, title : 'This is new card', category : 'done'
+            }
+        ]
+
+    },
+    methods : {
+        changePage (page) {
+            this.currentPage = page 
+        },
+        login() {
+            if (this.userData.password == 'secret') {
+                this.isLogin = true
+            } else {
+                this.isLogin = false
+            }
+        },
+        register () {
+            console.log(this.userData.name);
+            console.log(this.userData.email);
+            console.log(this.userData.password);
+        }
+
+    }
+})
