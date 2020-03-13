00a63357f6c6716eda6c0313068918c81cd84a4e
maxhamz <maxwell.hamzah@gmail.com>
2020-03-13 22:32:21 +0700

    google signin generated. Pending bug fixes

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="pg-landing" v-show="!isLogin">
-    <h1>看板ページにいらっしゃいます</h1>
+    <h1>看板ページ</h1>
     <form id="form-login">
       <h3>PLEASE LOGIN TO CONTINUE</h3>
       <div class="form-group">
@@ -42,7 +42,12 @@
           data-toggle="modal"
           data-target="#signupModal"
         >SIGNUP</button>
-        <div class="g-signin2" data-onsuccess="onSignIn"></div>
+        <g-signin-button
+          :params="googleSignInParams"
+          @success="onSignInSuccess"
+          @error="onSignInError"
+        > Sign in with Google</g-signin-button>
+        <!-- <div class="g-signin2" data-onsuccess="onSignIn"></div> -->
       </div>
     </form>
 
@@ -111,7 +116,10 @@ export default {
     return {
       userCreds: {
         email: "",
-        password: ""
+        password: "",
+      },
+      googleSignInParams: {
+        client_id: "983929968666-kbp86jo4o6053em2ioo0u6hbspvpnm4e.apps.googleusercontent.com"
       }
     };
   },
@@ -135,10 +143,34 @@ export default {
         password
       };
       this.$emit("userLogin1", userCreds);
+    },
+
+    onSignInSuccess (googleUser) {
+      console.log("GOOGLE SIGNIN STEP 1");
+      // `googleUser` is the GoogleUser object that represents the just-signed-in user.
+      // See https://developers.google.com/identity/sign-in/web/reference#users
+      var id_token = googleUser.getAuthResponse().id_token;
+      this.$emit("onSignInSuccess", id_token)
+    },
+    onSignInError (error) {
+      // `error` contains any error occurred.
+      console.log('OH NOES', error)
+      this.$toasted.error(error.response.data.message, {
+            position: "bottom-center"
+      });
     }
   }
 };
 </script>
 
-<style>
+<style scoped>
+.g-signin-button {
+  /* This is where you control how the button looks. Be creative! */
+  display: inline-block;
+  padding: 4px 8px;
+  border-radius: 3px;
+  background-color: #3c82f7;
+  color: #fff;
+  box-shadow: 0 3px 0 #0f69ff;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import axios from 'axios'
 import Toasted from "vue-toasted"
-// import GSignInButton from 'vue-google-signin-button'
+import GSignInButton from 'vue-google-signin-button'
 
 
 Vue.use(Toasted, {
   duration: 3000,
   position: "top-right",
 })
-// Vue.use(GSignInButton)
+Vue.use(GSignInButton)
 new Vue({
   render: h => h(App),
 }).$mount('#app');
