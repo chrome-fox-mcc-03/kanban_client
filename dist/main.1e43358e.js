ec9c8ada09e980f1e13656921fe4b670bc5760fd
Fadhilah Metra <fadhilahmetra@gmail.com>
2020-03-24 23:17:30 +0700

    Firebase config

--- dist/main.1e43358e.js
+++ dist/main.1e43358e.js
@@ -16397,7 +16397,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46511" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44195" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
